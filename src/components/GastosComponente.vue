6a5d5c05d5d810e27e45e616f981f7f8c9695758
mhmt78 <mhernan157@hotmail.com>
2020-07-08 16:42:35 -0500

    Mejorando componente gastos

--- src/components/GastosComponente.vue
+++ src/components/GastosComponente.vue
@@ -1,32 +1,28 @@
 <template>
-  <div>
+  
     <div class="row lead my-1">
       <div class="col-3">{{gasto.nombre}}</div>
       <div class="col-3">{{new Intl.NumberFormat("de-DE").format(gasto.monto)}}</div>
       <div class="col-3">{{gasto.tipo}}</div>
-      <div class="col-1">
-        <div
-          id="editar"
-          class="btn btn-primary"
+      <div class="col-3">
+        <div id="editar" class="btn btn-primary fa fa-pencil-square m-1"
           v-on:click="editar($event,{
             indice:indice, 
             id:id, 
             nombre: gasto.nombre,
             monto: gasto.monto,
             tipo: gasto.tipo,
-            mostrar: true
-          })"
-        >editar</div>
-      </div>
-      <div class="col-2">
+            mostrar: true})">
+        </div>
         <div
           id="eliminar"
-          class="btn btn-primary"
-          v-on:click="manejarClick($event,{indice:indice, id:id, monto: gasto.monto})"
-        >Eliminar</div>
+          class="btn btn-primary fa fa-trash m-1"
+          v-on:click="manejarClick($event,{indice:indice, id:id, monto: gasto.monto})">
+        </div>
       </div>
+      
     </div>
-  </div>
+  
 </template>
 
 <script>
