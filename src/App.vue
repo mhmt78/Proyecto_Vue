a489e6fb0fd966ccbe384005c20a8b2a12961f79
mhmt78 <mhernan157@hotmail.com>
2020-07-08 11:07:12 -0500

    Creando login y nuevo usuario

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id='login' v-if='!logon' >
+      <loginForm v-bind:firebase="firebase"
+                 v-on:ingresoCorrecto='ingresoCorrecto($event)'></loginForm>
+    </div>
+    <div id='inicio' v-else>
+      <div>Usuario: {{userData.usuario}} </div>
+      <div>Tipo de gasto: {{userData.gastos.tipo}} </div>
+      <div>Monto del Gastos: {{userData.gastos.monto}} </div>
+    </div>
+  </div> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import firebase from 'firebase'
+import 'firebase/firestore'
+import loginForm from './components/loginForm.vue';
 
 export default {
-  name: 'App',
+  name: 'app',
+  data: function (){
+    return {
+      logon:false,
+      coleccionRaiz:'usuarios',
+      idUsuario:'null',
+      coleccionGastos:'gastos',
+      userData:{},
+      firebase:'',
+      db:''
+    }
+  },
   components: {
-    HelloWorld
+    loginForm
+  },
+  beforeMount: function () {
+    var config = {
+      apiKey: "AIzaSyCrUxVyosflbJgmi1ZB9Jf3pru3ZAB2AuQ",
+      authDomain: "vueproyecto-8768f.firebaseapp.com",
+      databaseURL: "https://vueproyecto-8768f.firebaseio.com",
+      projectId: "vueproyecto-8768f",
+      storageBucket: "vueproyecto-8768f.appspot.com",
+      messagingSenderId: "379224037943",
+      appId: "1:379224037943:web:4e543933007a4aed627db2",
+      measurementId: "G-0LK6CLF9NX"
+    };
+    firebase.initializeApp(config);
+    this.db = firebase.firestore();
+    const settings = {timestampsInSnapshots: true};
+    this.db.settings(settings);
+    this.firebase=firebase
+  },
+  methods: {
+    ingresoCorrecto: function(usuario) {
+      console.log('User: '+usuario)
+      this.idUsuario=usuario
+      this.logon=true
+      const doc = this.db.collection(this.coleccionRaiz).doc(this.idUsuario)
+      this.userData={usuario:doc.id,gastos:{}}
+      const gastos = doc.collection(this.coleccionGastos)
+      const alquilerCasa = gastos.doc('alquiler_casa')
+      this.userData.gastos={tipo:alquilerCasa.id,monto:0}
+      alquilerCasa.get().then((doc)=>{
+      this.userData.gastos.monto=doc.data().monto
+    })
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
